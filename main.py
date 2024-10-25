5ac83025582aeb8ab01180f1d9edf65c86e51fa8
student239473 <239473@student.uek.krakow.pl>
2024-10-25 19:32:02 +0200

    Update main.py
    PUNKT 5

--- main.py
+++ main.py
@@ -126,3 +126,21 @@ height = 175
 print(f"My name is {name}.")
 print(f"I am {age} years old, and my height is {height} cm.")
 print(f"In 6 years I will be {age + 6} years old.")
+
+##################################################################################################
+
+father_income = 5450
+mother_income = 4920
+family_members = 5
+total_income = father_income + mother_income
+income_per_person = total_income / family_members
+print(f"Total family income is {total_income}, and income per person is {income_per_person:.2f}")
+
+#############################################################################################
+a = 3
+b = 5
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+##################################################################################################
