0f96d71d69ffd9bbaf08730f1a651f44ed1ef115
student239473 <239473@student.uek.krakow.pl>
2024-11-05 10:25:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -147,3 +147,10 @@ full_name = first_name + ' ' + last_name
 print(f'Your first name is {first_name} and your last name is {last_name}')
 print(f'And your full name is full_name')
 #################################################################
+cube_side_string = input('Enter cube side: ')
+cube_side = int(cube_side_string)
+cube_volume = cube_side ** 3
+cube_surface_area = 6 * (cube_side ** 2)
+print(f'The volume of a cube with side {cube_side} is {cube_volume}')
+print(f'The surface area of a cube with side {cube_side} is {cube_surface_area}')
+##############################################################
